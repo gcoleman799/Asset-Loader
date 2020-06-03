282bdcdd5459e4eb2824c6b927a803568a5e97ae
Grace Coleman <grcoleman@google.com>
2020-06-03 16:27:51 -0400

    attempt with button, not on one web view

--- app/src/main/java/com/example/android/exampleassetmanager/MainActivity.kt
+++ app/src/main/java/com/example/android/exampleassetmanager/MainActivity.kt
@@ -15,18 +15,24 @@
 //limitations under the License.
 package com.example.android.exampleassetmanager
 
+import android.media.Image
 import android.net.Uri
+import android.os.AsyncTask
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Button
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.webkit.WebViewAssetLoader
 import androidx.webkit.WebViewAssetLoader.AssetsPathHandler
 import com.example.android.exampleassetmanager.databinding.ActivityMainBinding
 import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
@@ -34,9 +40,18 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var textWebView: WebView
     private lateinit var photoWebView: WebView
-   
+    private lateinit var internalWebView: WebView
+    //from java doc
+
 
     inner class MyWebViewClient : WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+            if (view != null) {
+                view.loadUrl(url)
+            }
+            return true
+
+        }
 
         @RequiresApi(21)
         override fun shouldInterceptRequest(
@@ -63,6 +78,11 @@ class MainActivity : AppCompatActivity() {
         photoWebView.webViewClient = MyWebViewClient()
         textWebView.webViewClient = MyWebViewClient()
 
+        photoWebView.settings.javaScriptEnabled = true
+        photoWebView.settings.setSupportMultipleWindows(true)
+
+
+
 
 
         setTitle(R.string.app_name);
@@ -74,8 +94,8 @@ class MainActivity : AppCompatActivity() {
                 "/res/",
                 WebViewAssetLoader.ResourcesPathHandler(this)
             )
-
             .addPathHandler("/assets/", AssetsPathHandler(this))
+
             .build();
 
 
@@ -84,7 +104,7 @@ class MainActivity : AppCompatActivity() {
             .scheme("https")
             .authority(WebViewAssetLoader.DEFAULT_DOMAIN)
             .appendPath("assets")
-            //.appendPath("resources")
+            //.appendPath("res")
             //  .appendPath("www")
             .appendPath("myText.html")
             .build()
@@ -100,9 +120,10 @@ class MainActivity : AppCompatActivity() {
             .build()
 
 
-
-        textWebView.loadUrl(pathText.toString())
         photoWebView.loadUrl(pathPhoto.toString())
+        textWebView.loadUrl(pathText.toString())
+
+        
 
 
     }
@@ -115,5 +136,69 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+    fun loadNew(){
+        //set the path to the photo to display
+        val pathBernese = Uri.Builder()
+            .scheme("https")
+            .authority(WebViewAssetLoader.DEFAULT_DOMAIN)
+            .appendPath("res")
+            .appendPath("drawable")
+            //  .appendPath("www")
+            .appendPath("bernese.jpg")
+            .build()
+
+        photoWebView.loadUrl(pathBernese.toString())
+    }
+
+    fun doubleText(){
+        //set the path to the photo to display
+        val pathText2 = Uri.Builder()
+            .scheme("https")
+            .authority(WebViewAssetLoader.DEFAULT_DOMAIN)
+            .appendPath("assets")
+            //.appendPath("res")
+            //  .appendPath("www")
+            .appendPath("myText.html")
+            .build()
+
+
+        photoWebView.loadUrl(pathText2.toString())
+    }
+
+
+
+//    val button: Button = binding.switchButton
+//    button.setOnClickListener {
+//
+//        photoWebView.loadUrl(path.toString())
+//    }
+
 }
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        // Clean the test/demo file for tests.
+//        mDemoFile.delete()
+//        mPublicDir.delete()
+//    }
+//
+//    // Writes to file asynchronously in the background thread.
+//    private class WriteFileTask internal constructor(
+//        private val mFile: File,
+//        private val mContent: String
+//    ) :
+//        AsyncTask<Void?, Void?, Void?>() {
+//        protected override fun doInBackground(vararg params: Void): Void? {
+//            mFile.parentFile.mkdirs()
+//            try {
+//                FileOutputStream(mFile).use({ fos -> fos.write(mContent.getBytes(UTF_8)) })
+//            } catch (e: IOException) {
+//                throw RuntimeException(e)
+//            }
+//            return null
+//        }
+//
+//    }
+
+
 
