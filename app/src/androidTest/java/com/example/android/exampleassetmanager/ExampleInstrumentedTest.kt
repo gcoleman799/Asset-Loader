125e78ac001278748b1a02c91fd1bfcfe8b6e86a
Grace Coleman <grcoleman@google.com>
2020-06-08 11:29:58 -0400

    testing framework set up

--- app/src/androidTest/java/com/example/android/exampleassetmanager/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/android/exampleassetmanager/ExampleInstrumentedTest.kt
@@ -1,24 +1,67 @@
 package com.example.android.exampleassetmanager
 
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
 
+import android.app.PendingIntent.getActivity
+import androidx.test.espresso.web.assertion.WebViewAssertions.webMatches
+import androidx.test.espresso.web.model.Atoms.getCurrentUrl
+import androidx.test.espresso.web.sugar.Web
+import androidx.test.espresso.web.sugar.Web.onWebView
+import androidx.test.espresso.web.webdriver.DriverAtoms.findElement
+import androidx.test.espresso.web.webdriver.DriverAtoms.webClick
+import androidx.test.espresso.web.webdriver.Locator
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import com.google.android.material.internal.ContextUtils.getActivity
+import org.hamcrest.CoreMatchers.containsString
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
+
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
+private const val MACCHIATO = "Macchiato"
+private const val DOPPIO = "Doppio"
+
+@LargeTest
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+open class WebViewActivityTest {
+
+
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
+
+//        @get:Rule val activityScenarioRule =
+//            activityScenarioRule<MainActivity>()
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.html.android.exampleassetmanager", appContext.packageName)
+    fun testWebViewInteraction() {
+        onWebView().forceJavascriptEnabled()
+    }
+
+
+    @Test
+    fun checkLinkDestination() {
+        onWebView()
+            .withElement(findElement(androidx.test.espresso.web.webdriver.Locator.ID, "button")) // similar to onView(withId(...))
+            .perform(webClick()) // Similar to perform(click())
+
+            // Similar to check(matches(...))
+            .check(webMatches(getCurrentUrl(), containsString("https://www.w3docs.com/")))
     }
 }
+
+
+
+
+
+
+
+
